9e701e68c1f39ca409059c2e9241f346302b2e8a
Fabio Nolasco <gomidefabio@gmail.com>
2019-09-17 13:05:05 -0400

    Test DB

--- server.js
+++ server.js
@@ -8,25 +8,26 @@ const pool = mariadb.createPool({
     host: 'localhost',
     user: 'root',
     password: '123456',
-    db: 'employees',
+    database: 'employees',
     connectionLimit: 5
 });
 
 app.get('/', function (req, res) {
-    pool.getConnection()
-        .then(conn => {
-            console.log("connected ! connection id is " + conn.threadId);
-            conn.query('SELECT * FROM employess LIMIT 10')
-                .then(sqlRes => {
-                    res.json({
-                        response: sqlRes
-                    });
-            });
-            conn.release(); //release to pool
-        })
-        .catch(err => {
-            console.log("not connected due to error: " + err);
+
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const rows = await conn.query("SELECT * FROM employess LIMIT 10");
+        console.log(rows); //[ {val: 1}, meta: ... ]
+        res.json({
+            response: sqlRes
         });
+
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) return conn.end();
+    }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- start.sh
+++ start.sh
@@ -10,6 +10,12 @@ git clone https://github.com/datacharmer/test_db.git /var/www/test_db
 
 yum install -y gcc-c++ make
 
+yum install firewalld -y
+
+systemctl start firewalld
+
+systemctl enable firewalld
+
 curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash -
 
 yum install nodejs -y
@@ -36,3 +42,5 @@ systemctl status mariadb
 # firewall-cmd --permanent --add-port=3306/tcp
 
 firewall-cmd --add-port=80/tcp && firewall-cmd --permanent --add-port=80/tcp
+
+firewall-cmd --reload
